e15a1830152294b28ed50b9f1062c9facfc55ee8
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-07-20 13:46:34 +0200

    Use callbag composition instead of custom implementation

--- src/index.js
+++ src/index.js
@@ -1,38 +1,6 @@
+import concat from 'callbag-concat'
+import of from 'callbag-of'
+
 export default function concatWith(...args) {
-  return source => (start, sink) => {
-    if (start !== 0) return
-
-    let completed = false
-    let unsubed = false
-
-    source(0, (type, data) => {
-      if (type === 0) {
-        const talkback = data
-
-        sink(0, (type, data) => {
-          if (type === 2) {
-            unsubed = true
-            args.length = 0
-          }
-
-          if (completed) return
-
-          talkback(type, data)
-        })
-        return
-      }
-
-      if (type === 2 && !data) {
-        completed = true
-
-        while (args.length !== 0) {
-          sink(1, args.shift())
-        }
-
-        if (unsubed) return
-      }
-
-      sink(type, data)
-    })
-  }
+  return source => concat(source, of(...args))
 }
